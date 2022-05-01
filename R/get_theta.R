4288489fd21463e11390c57605498e05f0a6b3a9
Maggie Mao <mhuiying@vt.edu>
2022-05-01 10:08:58 -0700

    update get_theta

--- R/get_theta.R
+++ R/get_theta.R
@@ -3,7 +3,7 @@
 #' @param s spatial location
 #' @param Y data
 #' @param dists breakpoints for bins
-#' @param plot_fitted will plot a emperical variogram if specified as TRUE
+#' @param max_range maximum range
 #'
 #' @return Matern covariance parameters
 #' @export
@@ -12,48 +12,28 @@
 #' @importFrom dplyr left_join
 #' @keywords internal
 #'
-get_theta = function(s,Y,dists=NULL){
+get_theta <- function(s, Y, dists = NULL, max_range=NULL) {
 
-  if( length(Y) != nrow(s) )
-    stop( paste("the number of Y obs,", length(Y), ", does not match the number of locations,", nrow(s)) )
+  if(is.null(max_range)){max_range=max(dist(s))}
+  vario = variog(coords = s, data = Y, uvec = dists, messages = FALSE)
 
-  if(length(Y) < 10^6){
-    vario = variog(coords=s,data=Y,uvec=dists,messages=FALSE)
-  } else {
-    s1       = sort(unique(s[,1]))
-    s2       = sort(unique(s[,2]))
-    s_full   = expand.grid(x=s1,y=s2)
-    sY       = as.data.frame(cbind(s, Y))
-    sY_full  = left_join(s_full, sY, by = c("x" = "x", "y" = "y"))
-    s        = sY_full[, 1:2]
-    Y        = sY_full[, 3]
-
-    vario = list( u=c(), v=c(), n=c() )
-    s1    = sort(unique(s[,1]))
-    n_s1  = length(s1)
-    diff_s1 = s1[2]-s1[1]
-    s2    = sort(unique(s[,2]))
-    n_s2  = length(s2)
-    Y_mat = matrix(Y, nrow = n_s1) # Y in matrix format
-
-    for( NN in 1:10){
-      D = c(Y_mat[1:(n_s1-NN),]-Y_mat[(NN+1):n_s1,], Y_mat[,1:(n_s2-NN)]-Y_mat[,(NN+1):n_s2])
-      D = D[!is.na(D)]
-      vario$n = c( vario$n, length(D) )
-      vario$u = c( vario$u, diff_s1*NN )
-      vario$v = c( vario$v, mean(D^2) )
-    }
-  }
-
-  wsse = function(logtheta,u,v,w){
+  wsse  = function(logtheta, u, v, w, max_range) {
     theta = exp(logtheta)
-    m     = theta[1]+theta[2]-mat_cov(u,exp(logtheta))
-    out   = sum(w*(v-m)^2)
+    theta[4] = max_range*pnorm(logtheta[4])
+    m     = theta[1] + theta[2] - theta[2]*matern(u,theta[3],theta[4])
+    out   = sum(w * (v - m)^2)
     return(out)}
 
-  init  = c(0.5*var(Y,na.rm=TRUE),0.5*var(Y,na.rm=TRUE),median(vario$u),0.5)
-  fit   = optim(log(init),wsse,u=vario$u,v=vario$v,w=vario$n)
+  init  = c(0.5 * var(Y, na.rm = TRUE), 0.5 * var(Y, na.rm = TRUE),
+            quantile(vario$u,0.5), 0.5)
+
+  fit   = optim(log(init), wsse, u = vario$u, v = vario$v,
+                w = vario$n, max_range=max_range)
+
   theta = exp(fit$par)
-  names(theta) = c("Nugget","PartialSill","Range","Smoothness")
+
+  theta[4] = max_range*pnorm(fit$par[4])
+
+  names(theta) = c("Nugget", "PartialSill", "Range", "Smoothness")
 
   return(theta)}
